5fd4eb1492f7fc64c3284ea357c49f2029653e99
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2025-02-27 13:27:24 -0300

    troca de cor de letra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Sobre from '@/components/sections/Sobre';
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full bg-gradient-to-r from-[#AAAAAA] to-slate-50 ">
+    <main className="flex flex-col w-full bg-gradient-to-r from-[#AAAAAA] to-slate-50 text-zinc-700">
       <Menu />
       <Banner />
       <Sobre />
